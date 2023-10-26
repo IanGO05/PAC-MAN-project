04dc7d4a6edccc8ad3ad0b73911f629bc77057a7
IanGO05 <iagomez@estudiantec.cr>
2023-10-25 23:21:43 -0600

    import

--- PacMandef.py
+++ PacMandef.py
@@ -1,4 +1,13 @@
 import tkinter as tk
+from tkinter import *
+from threading import Thread
+import random
+import time
+from PIL import ImageTk, Image
+from tkinter import Toplevel
 import pygame
-import abc
-#hola
+import natsort
+from tkinter import font, PhotoImage
+from natsort import natsorted
+from tkinter import messagebox
+
